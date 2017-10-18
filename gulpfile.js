6f53427ef1c8a13e3dbf6011ec9d71fb47c29350
Matthew Lean <Matthew Lean>
2017-10-18 20:56:42 +0100

    started cleaning up the styles.scss

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
         .on('close', done);
+        --incremental
 });
 
 /**
